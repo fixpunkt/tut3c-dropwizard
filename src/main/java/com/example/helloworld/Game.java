4b91dcc6fc85ec9c0316974a42811003448279e2
hans-peter-ingo@gmx.de <hans-peter-ingo@gmx.de>
2015-01-17 14:09:17 +0100

    gamefield

--- src/main/java/com/example/helloworld/Game.java
+++ src/main/java/com/example/helloworld/Game.java
@@ -5,36 +5,55 @@ import java.util.stream.Stream;
 
 public class Game {
 
-        public long getId() {
-                return id;
-        }
+	private long id;
+	private Cell[][] cells;
 
-        private long id;
-        private Cell[][] cells;
+	public Game(int id) {
+		this.id = id;
+		reset();
+	}
 
-        public Game(int id) {
-                this.id = id;
-                reset();
-        }
+	public long getId() {
+		return id;
+	}
+	
+	public void reset() {
+		cells = new Cell[][] {
+				new Cell[] { Cell.EMPTY, Cell.EMPTY, Cell.EMPTY },
+				new Cell[] { Cell.EMPTY, Cell.EMPTY, Cell.EMPTY },
+				new Cell[] { Cell.EMPTY, Cell.EMPTY, Cell.EMPTY }, };
+	}
 
-        public void reset() {
-                cells = new Cell[][] {
-                                new Cell[] { Cell.EMPTY, Cell.EMPTY, Cell.EMPTY },
-                                new Cell[] { Cell.EMPTY, Cell.EMPTY, Cell.EMPTY },
-                                new Cell[] { Cell.EMPTY, Cell.EMPTY, Cell.EMPTY }, };
-        }
+	public Cell[][] getCells() {
+		return cells;
+	}
 
-        public Cell[][] getCells() {
-                return cells;
-        }
+	public void setCell(int i, int j, Cell x) {
+		if (isSet(i, j)) {
+			throw new IllegalStateException();
+		}
+		cells[i][j] = x;
+	}
 
-        @Override
-        public String toString() {
-                Stream<Object> map = Arrays.stream(cells).map(array -> {
-                        return Arrays.stream(array).map(Object::toString).reduce((x, y) -> x + ", " + y);
-                });
-                map.reduce((x, y) -> x + "\n" +  y);
-                return "";
-        }
+	public boolean isSet(int i, int j) {
+		return cells[i][j] != Cell.EMPTY;
+	}
+
+	public Cell getCell(int i, int j) {
+		return cells[i][j];
+	};
+	
+	@Override
+	public String toString() {
+		Stream<Object> map = Arrays.stream(cells).map(
+				array -> {
+					return Arrays.stream(array).map(Object::toString)
+							.reduce((x, y) -> x + "|" + y).get();
+				});
+		String determinator = "+-+-+-+";
+		return (String) map.map(l -> determinator + "\n|" + l + "|")
+				.reduce((x, y) -> x + "\n" + y).get()
+				+ "\n" + determinator;
+	}
+
 }
-
--- src/test/java/com/example/helloworld/GameTest.java
+++ src/test/java/com/example/helloworld/GameTest.java
@@ -8,8 +8,33 @@ public class GameTest {
 	public void test() {
 		Game game = new Game(0);
 		Assert.assertEquals(0, game.getId());
-		System.out.println(game.toString());
-		System.out.println("asdf");
+		
+		String emptyField = "+-+-+-+\n| | | |\n+-+-+-+\n| | | |\n+-+-+-+\n| | | |\n+-+-+-+";
+		Assert.assertEquals(emptyField, game.toString());
+		
+		game.setCell(0, 0, Cell.X);
+		game.setCell(1, 1, Cell.X);
+		game.setCell(2, 2, Cell.X);
+		game.setCell(2, 1, Cell.O);
+		
+		Assert.assertEquals(Cell.O, game.getCell(2, 1));
+		
+		Assert.assertEquals(
+				"+-+-+-+\n|X| | |\n+-+-+-+\n| |X| |\n+-+-+-+\n| |O|X|\n+-+-+-+",
+				game.toString());
+		
+		boolean exceptionThrown = false;
+		try {
+			game.setCell(0, 0, Cell.X);
+		} catch (IllegalStateException ex) {
+			exceptionThrown = true;
+		}
+		Assert.assertTrue(
+				"Es müsste das setzen eines Feldes zum zweiten Mal zu einem Fehler führen!",
+				exceptionThrown);
+
+		game.reset();
+		Assert.assertEquals(emptyField, game.toString());
 	}
-	
+
 }
