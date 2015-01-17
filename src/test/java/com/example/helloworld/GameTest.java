e004467bf97bb41cb482474b06d6c9d876a1d40a
hans-peter-ingo@gmx.de <hans-peter-ingo@gmx.de>
2015-01-17 14:09:17 +0100

    gamefield

--- src/test/java/com/example/helloworld/GameTest.java
+++ src/test/java/com/example/helloworld/GameTest.java
@@ -8,8 +8,31 @@ public class GameTest {
 	public void test() {
 		Game game = new Game(0);
 		Assert.assertEquals(0, game.getId());
-		System.out.println(game.toString());
-		System.out.println("asdf");
+		
+		String emptyField = "+-+-+-+\n| | | |\n+-+-+-+\n| | | |\n+-+-+-+\n| | | |\n+-+-+-+";
+		Assert.assertEquals(emptyField, game.toString());
+		
+		game.setValue(0, 0, Cell.X);
+		game.setValue(1, 1, Cell.X);
+		game.setValue(2, 2, Cell.X);
+		game.setValue(2, 1, Cell.O);
+		
+		Assert.assertEquals(
+				"+-+-+-+\n|X| | |\n+-+-+-+\n| |X| |\n+-+-+-+\n| |O|X|\n+-+-+-+",
+				game.toString());
+		
+		boolean exceptionThrown = false;
+		try {
+			game.setValue(0, 0, Cell.X);
+		} catch (IllegalStateException ex) {
+			exceptionThrown = true;
+		}
+		Assert.assertTrue(
+				"Es müsste das setzen eines Feldes zum zweiten Mal zu einem Fehler führen!",
+				exceptionThrown);
+
+		game.reset();
+		Assert.assertEquals(emptyField, game.toString());
 	}
-	
+
 }
