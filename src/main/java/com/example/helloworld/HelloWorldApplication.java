80be9a5a4462bf245405dea92f7f9cbe24da15e5
Martin Steiger <martin.steiger@igd.fraunhofer.de>
2015-01-17 11:19:46 +0100

    First setup

---
+++ src/main/java/com/example/helloworld/HelloWorldApplication.java
@@ -0,0 +1,29 @@
+package com.example.helloworld;
+
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class HelloWorldApplication extends Application<HelloWorldConfig> {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello DropWizard");
+        new HelloWorldApplication().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return "hello-world";
+    }
+
+    @Override
+    public void initialize(Bootstrap<HelloWorldConfig> bootstrap) {
+        // nothing to do yet
+    }
+
+    @Override
+    public void run(HelloWorldConfig configuration, Environment environment) {
+        // nothing to do yet
+    }
+
+}
